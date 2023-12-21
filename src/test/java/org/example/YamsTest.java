7e63278130fea525dbb0fe80f18f515b01c89769
Thavfyk2 <tabdoulazis@myges.fr>
2023-12-21 12:24:52 +0100

    Test for valid roll

--- src/test/java/org/example/YamsTest.java
+++ src/test/java/org/example/YamsTest.java
@@ -4,14 +4,23 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 class YamsTest {
-
     int[] roll1 = {1,2,3,5,6};
     int[] grandeSuite = {2,3,4,5,6};
 
     @Test
     void isGrandeSuiteTest(){
         Assertions.assertTrue(Yams.isGrandeSuite(grandeSuite));
+    }
+
+    @Test
+    void isNotGrandeSuiteTest(){
         Assertions.assertFalse(Yams.isGrandeSuite(roll1));
     }
+
+    @Test
+    void validRollTest(){
+        Assertions.assertFalse(Yams.validRoll(roll1));
+    }
+
 
 }
