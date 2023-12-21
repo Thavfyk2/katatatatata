ffea40d8f6ae7b6b5b3be265b583d10d0ac84da5
Thavfyk2 <tabdoulazis@myges.fr>
2023-12-21 12:23:51 +0100

    isGrandeSuite is implemented and tested

--- src/main/java/org/example/Yams.java
+++ src/main/java/org/example/Yams.java
@@ -1,7 +1,14 @@
 package org.example;
 
 public class Yams {
-
-
+    
+    public static boolean isGrandeSuite(int[] roll){
+        for (int i = 0; i < roll.length - 1; i++) {
+            if (roll[i] != roll[i + 1] - 1) {
+                return false;
+            }
+        }
+        return true;
+    }
 
 }
