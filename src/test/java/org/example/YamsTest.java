a645aedebcbb92f4b0544a4164cfb58cfffdaea3
Thavfyk2 <tabdoulazis@myges.fr>
2023-12-21 12:12:52 +0100

    Test of Grande Suite

--- src/test/java/org/example/YamsTest.java
+++ src/test/java/org/example/YamsTest.java
@@ -1,10 +1,17 @@
 package org.example;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 class YamsTest {
 
+    int[] roll1 = {1,2,3,5,6};
+    int[] grandeSuite = {2,3,4,5,6};
+
+    @Test
+    void isGrandeSuiteTest(){
+        Assertions.assertTrue(Yams.isGrandeSuite(grandeSuite));
+        Assertions.assertFalse(Yams.isGrandeSuite(roll1));
+    }
 
 }
