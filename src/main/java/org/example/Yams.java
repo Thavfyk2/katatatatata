a009bf5a1f6861b76c521e0b63971eebc75432f9
Thavfyk2 <tabdoulazis@myges.fr>
2023-12-21 12:37:54 +0100

    isBrelan implemented

--- src/main/java/org/example/Yams.java
+++ src/main/java/org/example/Yams.java
@@ -1,5 +1,7 @@
 package org.example;
 
+import java.util.Arrays;
+
 public class Yams {
     
     public static boolean isGrandeSuite(int[] roll){
@@ -11,4 +13,18 @@ public class Yams {
         return true;
     }
 
+    public static boolean validRoll(int[] roll) {
+        return roll.length == 6;
+    }
+
+    public static boolean isBrelan(int[] roll) {
+        Arrays.sort(roll);
+
+        for (int i = 0; i <= roll.length - 3; i++) {
+            if (roll[i] == roll[i + 1] && roll[i] == roll[i + 2]) {
+                return true;
+            }
+        }
+        return false;
+    }
 }
